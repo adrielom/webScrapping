df58aa945ff4f03a91e6ce3eacd9853900bf1799
adrielom <adrielom.dev@gmail.com>
2020-08-20 22:59:18 -0300

    headless gone

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.listen(PORT, () => {
 
 async function CalculateDelivery(address1, address2) {
     try {
-        const browser = await puppeteer.launch({ headless: false }   /*this opens up a browser*/);
+        const browser = await puppeteer.launch(/*{ headless: false }   this opens up a browser*/);
         const page = await browser.newPage();
 
         await page.goto('https://ligmototaxi.com.br/simulacao?tipo=M');
