8dc0df5dea590cc106cf21d196f11769681089c5
adrielom <adrielom.dev@gmail.com>
2020-08-20 23:21:05 -0300

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const express = require('express')
 
 const app = express()
-const PORT = 5600
+const PORT = process.env.PORT || 5600
 
 
 app.get('/', async (req, res) => {
